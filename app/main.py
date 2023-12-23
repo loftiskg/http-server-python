152e8b5790ed8edfffca8d5ce7c51728931f82eb
Kevin Loftis <kevin.loftis@whatnot.com>
2023-12-23 14:38:35 -0600

    Add Response class and refactor response handling

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import argparse
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 import re
 import socket
@@ -9,6 +9,7 @@ from typing import Dict
 
 BUFFER_SIZE = 1024
 DIRECTORY: Path | None = None
+CLRF = '\r\n'
 
 NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
 
@@ -19,6 +20,22 @@ class Request:
     protocol: str
     headers: Dict[str, str]
     body: str
+
+@dataclass
+class Response:
+    protocol: str = "HTTP/1.1"
+    status_code: int = 200
+    status_text: str = 'OK'
+    headers: Dict[str, str] = field(default_factory=dict)
+    body: str | None = None
+
+
+def response_200_ok():
+    return Response(status_code=200, status_text='OK')
+def response_404_not_found():
+    return Response(status_code=404, status_text='Not Found')
+def response_201_created():
+    return Response(status_code=201, status_text='Created')
 
 def parse_http_response(request_data: str) -> Request:
     lines = request_data.splitlines()
@@ -36,6 +53,20 @@ def parse_http_response(request_data: str) -> Request:
 
     return Request(method, path, protocol, headers, body)
 
+def encode_http_response(response: Response):
+    protocol, status_code, status_text = response.protocol, response.status_code, response.status_text
+    start_line = f"{protocol} {status_code} {status_text}"
+    response_encoded = [start_line]
+
+    if response.headers:
+        response_encoded += [f"{key}: {value}" for (key, value) in response.headers.items()]
+    if response.body:
+        response_encoded.append('')
+        response_encoded.append(response.body)     
+
+    return (CLRF.join(response_encoded) + CLRF).encode()
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -51,7 +82,7 @@ def main():
 
 
 
-def handler(connection, address):
+def handler(connection: socket.socket, address: str):
     # receive client's request
     request_data = connection.recv(BUFFER_SIZE).decode()
     request = parse_http_response(request_data)
@@ -68,23 +99,22 @@ def handler(connection, address):
         resp = not_found_handler(request)
 
     # send response to client
-    connection.sendall(resp.encode())
+    connection.sendall(encode_http_response(resp))
 
     connection.close()
 
 
 
-def root_handler(path: Request):
-    return "HTTP/1.1 200 OK\r\n\r\n"
+def root_handler(path: Request) -> Response:
+    return response_200_ok()
 
 def echo_handler(request: Request):
     body = re.match(r"/echo/(.*)", request.path).group(1)
 
-    response = "HTTP/1.1 200 OK\r\n" +\
-               "Content-Type: text/plain\r\n" +\
-               "Content-Length: {}\r\n".format(len(body)) +\
-               "\r\n" +\
-               "{}\r\n".format(body)
+    response = response_200_ok()
+    response.headers['Content-Type'] = 'text/plain'
+    response.headers['Content-Length'] = '{}'.format(len(body))
+    response.body = body
 
     return response
 
@@ -94,11 +124,10 @@ def not_found_handler(path: Request):
 def user_agent_handler(request: Request):
     user_agent = request.headers["User-Agent"]
 
-    response = "HTTP/1.1 200 OK\r\n" +\
-               "Content-Type: text/plain\r\n" +\
-               "Content-Length: {}\r\n".format(len(user_agent)) +\
-               "\r\n" +\
-               "{}\r\n".format(user_agent)
+    response = response_200_ok()
+    response.headers['Content-Type'] = 'text/plain'
+    response.headers['Content-Length'] = '{}'.format(len(user_agent))
+    response.body = user_agent
 
     return response
 
@@ -109,17 +138,15 @@ def files_handler(request: Request):
     else:
         fpath = DIRECTORY/filename
         if fpath.is_file():
-            resp_body = None
+            resp = response_200_ok()
             with fpath.open() as f:
-                resp_body = f.read()
-            return "HTTP/1.1 200 OK\r\n" +\
-                "Content-Type: application/octet-stream\r\n" +\
-                "Content-Length: {}\r\n".format(len(resp_body)) +\
-                "\r\n" +\
-                "{}\r\n".format(resp_body)
+                resp.body = f.read()
+            resp.headers['Content-Type'] = 'application/octet-stream'
+            resp.headers['Content-Length'] = '{}'.format(len(resp.body))
+            return resp
         else:
             print(f"Requested file not found: {fpath}")
-            return NOT_FOUND
+            return response_404_not_found()
   
             
 
